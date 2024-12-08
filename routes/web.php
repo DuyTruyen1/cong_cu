df5a1507e78056346214551a48cca37f865222df
DuyTruyen1 <truyenmap420@gmail.com>
2024-12-08 10:34:49 +0700

    ADMIN

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => '/admin'], function () { //, 'middleware' => 'authadmin'
+    Route::get('/', [AdminController::class, 'viewHome']);
+    Route::get('/logout', [AdminController::class, 'logout']);
 });
